91d49f7eb5677463b47ee22dce660ff45b0bb5ab
denis-grossman <67560103+denis-grossman@users.noreply.github.com>
2024-04-16 13:39:41 +0200

    Update Program.cs

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -2,7 +2,7 @@
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-var text = "Денис - педик";
+var text = "Коля - педик";
 Console.WriteLine($"Translate sync. {text} : {PENIS.Translate(text, "ru", "en")}");
 Console.WriteLine($"Translate async. {text} : {await PENIS.TranslateAsync(text)}");
 
