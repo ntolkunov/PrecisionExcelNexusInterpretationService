858a8dce0a6aee7f04bbe3db8807c185e80997a9
ntolkunov <nikolaytolkunov@gmail.com>
2024-04-17 14:54:31 +0400

    translate list of strings + retry limit

--- PrecisionExcelNexusInterpretationService/PENIS.cs
+++ PrecisionExcelNexusInterpretationService/PENIS.cs
@@ -9,32 +9,92 @@ namespace PrecisionExcelNexusInterpretationService
 {
     public class PENIS
     {
-        public static string Translate(string text, string langFrom = "auto", string langTo = "auto")
-        {  
-            var url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={
-                langFrom}&tl={langTo}&dt=t&q={
-                HttpUtility.UrlEncode(text)}";
-
+        private static string TranslateInternal(string text, string langFrom = "auto", string langTo = "auto", int retryLimit = 3)
+        {
+            var url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={langFrom}&tl={langTo}&dt=t&q={HttpUtility.UrlEncode(text)}";
             using (var client = new HttpClient())
             {
-                var json = client.GetStringAsync(url).GetAwaiter().GetResult();
-                var translation = JArray.Parse(json)?.First?.First?.First?.ToString(); 
-                return translation ?? string.Empty;
+                int retryCount = 0;
+                while (retryCount < retryLimit)
+                {
+                    try
+                    {
+                        var json = client.GetStringAsync(url).GetAwaiter().GetResult();
+                        var translation = JArray.Parse(json)?.First?.First?.First?.ToString();
+                        return translation ?? string.Empty;
+                    }
+                    catch (Exception ex)
+                    {
+                        retryCount++;
+                    }
+                }
+
+                if (retryCount == retryLimit)
+                    throw new Exception("Retry limit exceeded");
             }
+
+            return string.Empty;
         }
 
-        public static async Task<string> TranslateAsync(string text, string langFrom= "auto", string langTo = "auto")
+        private static async Task<string> TranslateInternalAsync(string text, string langFrom = "auto", string langTo = "auto", int retryLimit = 3)
         {
-            var url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={
-                langFrom}&tl={langTo}&dt=t&q={
-                HttpUtility.UrlEncode(text)}";
-
+            var url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={langFrom}&tl={langTo}&dt=t&q={HttpUtility.UrlEncode(text)}";
             using (var client = new HttpClient())
             {
-                var json = await client.GetStringAsync(url);
-                var translation = JArray.Parse(json)?.First?.First?.First?.ToString();
-                return translation ?? string.Empty;
+                int retryCount = 0;
+                while (retryCount < retryLimit)
+                {
+                    try
+                    {
+                        var json = await client.GetStringAsync(url);
+                        var translation = JArray.Parse(json)?.First?.First?.First?.ToString();
+                        return translation ?? string.Empty;
+                    }
+                    catch (Exception ex)
+                    {
+                        retryCount++;
+                    }
+                }
+
+                if (retryCount == retryLimit)
+                    throw new Exception("Retry limit exceeded");
             }
+
+            return string.Empty;
+        }
+
+        public static string Translate(string text, string langFrom = "auto", string langTo = "auto", int retryLimit = 3)
+        {  
+            return TranslateInternal(text, langFrom, langTo, retryLimit);
+        }
+
+        public static List<string> Translate(List<string> textList, string langFrom = "auto", string langTo = "auto", int retryLimit = 3)
+        {
+            var result = new List<string>();
+
+            foreach (var text in textList)
+            {
+                result.Add(TranslateInternal(text, langFrom, langTo, retryLimit));
+            }
+            
+            return result;
+        }
+
+        public static async Task<string> TranslateAsync(string text, string langFrom= "auto", string langTo = "auto", int retryLimit = 3)
+        {
+            return await TranslateInternalAsync(text, langFrom, langTo, retryLimit);
+        }
+
+        public static async Task<List<string>> TranslateAsync(List<string> textList, string langFrom = "auto", string langTo = "auto", int retryLimit = 3)
+        {
+            var result = new List<string>();
+
+            foreach (var text in textList)
+            {
+                result.Add(await TranslateInternalAsync(text, langFrom, langTo, retryLimit));
+            }
+
+            return result;
         }
     }
 }
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -3,7 +3,28 @@
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 var text = "Денис - педик";
+var textList = new List<string>()
+{
+    "Денис - первый педик",
+    "Денис - средний педик",
+    "Денис - последний педик"
+};
+
 Console.WriteLine($"Translate sync. {text} : {PENIS.Translate(text, "ru", "en")}");
 Console.WriteLine($"Translate async. {text} : {await PENIS.TranslateAsync(text)}");
+
+var translatedList = PENIS.Translate(textList);
+
+for(int i = 0; i < textList.Count; i++)
+{
+    Console.WriteLine($"Translate list sync. Item {i}. {textList[i]} : {translatedList[i]}");
+}
+
+translatedList = await PENIS.TranslateAsync(textList);
+
+for (int i = 0; i < textList.Count; i++)
+{
+    Console.WriteLine($"Translate list async. Item {i}. {textList[i]} : {translatedList[i]}");
+}
 
 Console.ReadKey();
